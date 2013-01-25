e74c393ad9827c91d7eb445182c885396acdb426
Marc <mj@cquier.com>
2013-01-25 13:44:40 +0100

    Add Mag

--- include/msg/MessageSensor.h
+++ include/msg/MessageSensor.h
@@ -5,7 +5,7 @@
  # SENSORS
  #--------------------------------------------------*/
 
-#define UAVLINK_MSG_SENSOR_LEN 18
+#define UAVLINK_MSG_SENSOR_LEN 24
 typedef struct __uavlink_message_sensor {
     int16_t accX;
     int16_t accY;
@@ -13,6 +13,9 @@ typedef struct __uavlink_message_sensor {
     int16_t gyroX;
     int16_t gyroY;
     int16_t gyroZ;
+    int16_t magX;
+    int16_t magY;
+    int16_t magZ;
     int16_t roll;
     int16_t pitch;
     int16_t yaw;
@@ -28,9 +31,12 @@ inline static void uavlink_message_sensor_decode(const uavlink_message_t* msg, u
     sensor->gyroX = _UAVLINK_RETURN_uint16_t(msg,6);
     sensor->gyroY = _UAVLINK_RETURN_uint16_t(msg,8);
     sensor->gyroZ = _UAVLINK_RETURN_uint16_t(msg,10);
-    sensor->roll = _UAVLINK_RETURN_uint16_t(msg,12);
-    sensor->pitch = _UAVLINK_RETURN_uint16_t(msg,14);
-    sensor->yaw = _UAVLINK_RETURN_uint16_t(msg,16);
+    sensor->magX = _UAVLINK_RETURN_uint16_t(msg,12);
+    sensor->magY = _UAVLINK_RETURN_uint16_t(msg,14);
+    sensor->magZ = _UAVLINK_RETURN_uint16_t(msg,16);
+    sensor->roll = _UAVLINK_RETURN_uint16_t(msg,18);
+    sensor->pitch = _UAVLINK_RETURN_uint16_t(msg,20);
+    sensor->yaw = _UAVLINK_RETURN_uint16_t(msg,22);
 #else
     memcpy(sensor, _UAVLINK_PAYLOAD(msg), UAVLINK_MSG_SENSOR_LEN);
 #endif
